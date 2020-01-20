72a1ec3db6096610780426f92d23463de4a4e3bc
Buddy <BuddyBoi@users.noreply.github.com>
2020-01-19 20:59:01 -0500

    Create fiddler.cs

---
+++ fiddler.cs
@@ -0,0 +1,10 @@
+//Runs the program through a proxy.
+void AntiFiddler() => HttpWebRequest.DefaultWebProxy = new WebProxy();
+
+//Alternatively, and more efficiently but more work, we can also wrap only the defined web request in the proxy,
+//closing it afterwards like so:
+
+using(WebClient wc = new WebClient()) {
+wc.DownloadString("foo.bar");
+wc.Proxy = null;
+}
